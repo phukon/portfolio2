7a9842201439eb454940286975f542ae8cace4ea
phukon <rikiphukon16@gmail.com>
2023-01-21 08:33:00 +0530

    idk wuts happening

--- src/client.js
+++ src/client.js
@@ -2,5 +2,7 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
     projectId:'7oazl9jk',
-    dataset: 'production'
+    dataset: 'production',
+    apiVersion: '2022-11-15',
+    useCdn: true,
 })
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
 import sanityClient from '../client'
 
-const posts = () => {
+const Posts = () => {
   const [postData, setPost] = useState(null);
 
   useEffect(() => {
@@ -18,7 +19,30 @@ const posts = () => {
       }`)
       .then((data) => setPost(data))
       .catch(console.error);
-  }, [])
-}
+  }, []);
 
-export default posts
+  return(
+    <main className=' bg-green-100 min-h-screen p-12'>
+      <section className=' container mx-auto'>
+        <h1 className=' text-5xl flex justify-center'>Blog posts page</h1>
+        <h2 className=' text-lg text-gray-600 flex justify-center mb-12'>Welcome to the page of my blog posts</h2>
+        <div className=' grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          {postData && postData.map((post, index) => (
+            <article>
+              <Link to={'/post/' + post.slug.current} key={post.slug.current}>
+                <span className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400' key={index}>
+                  <img src={post.mainImage.asset.url} alt={post.mainImage.alt} className=' w-full -full rounded-r object-cover absolute'/>
+                  <span className=' block relative h-full flex justify-end items-end pr-4 pb-4'>
+                    <h3 className=' text-gray-800 text-lg px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded'>{post.title}</h3>
+                  </span>
+                </span>
+              </Link>
+            </article>
+          ))}
+        </div>
+      </section>
+    </main>
+  )
+  }
+
+export default Posts
