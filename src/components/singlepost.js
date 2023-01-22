02b14d76fd6c75f37ac75cdf615d7954cba5a950
phukon <rikiphukon16@gmail.com>
2023-01-22 18:15:15 +0530

    dafq is happening?!

--- src/components/singlepost.js
+++ src/components/singlepost.js
@@ -51,12 +51,12 @@ const Singlepost = () => {
               </div>
             </div>
           </div>
-          <img src={Singlepost.mainImage.asset.url} alt={singlePost.title}
+          <img src={singlePost.mainImage.asset.url} alt={singlePost.title}
           className='w-full object-cover rounded-t'
           style={{height: '400px'}}/>
         </header>
         <div className=' px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full'>
-          <BlockContent blocks={singlePost.body} projectId='7oazl9jk' dataset='production'></BlockContent></div>
+          <BlockContent blocks={singlePost.body} projectId='7oazl9jk' dataset='production'/></div>
       </article>
     </main>
   )
