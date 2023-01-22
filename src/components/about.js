89f863b48bd4bb48d48728377f3fda5f37bc58d1
phukon <rikiphukon16@gmail.com>
2023-01-22 18:53:30 +0530

    done

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import sanityClient from '../client'
+import fuck from '../assets/images'
+import imageUrlBuilder from '@sanity/image-url'
+import BlockContent from '@sanity/block-content-to-react'
 
-const about = () => {
+
+const builder = imageUrlBuilder(sanityClient)
+function urlFor(source) {
+  return builder.image(source)
+}
+
+const About = () => {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == 'author']{
+      name,
+      bio,
+      'authorImage': image.asset->url
+    }`).then((data) => setAuthor(data[0]))
+    .catch(console.error);
+  }, []);
+
+  if (!author) return <div>Loading... .. .</div>;
+
   return (
-    <div>about page</div>
+    <main className=' relative'>
+      <img src={fuck.bgpic} alt='bg pic' className=' absolute w-full'/>
+      <div className=' p-10 lg:pt-48 container mx-auto relative'>
+        <section className=' bg-green-800 rounded-lg shadow-2xl lg:flex p-20'>
+          <img src={urlFor(author.authorImage).url()} className=' rounded w-32 h-32 lg:w-64 mr-8' alt={author.name}/>
+          <div className='text-lg flex flex-col justify-center'>
+            <h1 className=' text-6xl text-green-300 mb-4'>
+              Hey there. I'm {''}
+              <span className=' text-green-100'>{author.name}</span>
+            </h1>
+            <div className=' prose lg:prose-xl text-white'>
+              <BlockContent blocks={author.bio} projectId='7oazl9jk' dataset= 'production'/>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
 
-export default about
+export default About
